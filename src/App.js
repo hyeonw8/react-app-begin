3c729ad592eadd3c0b6bcaa437262be8a135a5ce
hyeonw8 <whgusrud223@gmail.com>
2023-05-19 23:56:19 +0900

    #3 state, key

--- src/App.js
+++ src/App.js
@@ -13,12 +13,25 @@ import './App.css';
 // }
 
 class App extends Component { // class type
+  constructor(props){
+    super(props);
+    this.state = {
+      subject:{title:'WEB', sub:'World Wide Web!'},
+      contents:[
+        {id:1, title:'WEB', desc:'HTMl is for information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+      ]
+    }
+  }
   render() { // render 메소드
     return (
       <div className='App'>
-        <Subject title="WEB" sub="world wide web!"></Subject>
-        <TOC></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}></Subject>
+        <TOC data={this.state.contents}></TOC>
+        <Content title="HTML" desc="HTML is Hype rText Markup Language."></Content>
       </div>
     )
   }
