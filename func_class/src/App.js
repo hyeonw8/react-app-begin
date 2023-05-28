5d57ab9199af1351bf15dd2836b55a6718a3c305
hyeonw8 <whgusrud223@gmail.com>
2023-05-29 00:00:20 +0900

    #1.1~2 state 사용법과 hook

--- func_class/src/App.js
+++ func_class/src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      <ClassComp initNumber={10} />
+      <FuncComp initNumber={20} />
+    </div>
+  );
+}
+
+class ClassComp extends React.Component {
+  state = {
+    number: this.props.initNumber,
+    date: (new Date().toString())
+  }
+  render() {
+    return (
+      <div className="container">
+        <h2>class style component</h2>
+        <p>Class Component Number: {this.state.number}</p>
+        <p>Class Component Date: {this.state.date}</p>
+        <input type="button" value="random1" onClick={
+          function() {
+            this.setState({number: Math.random()})
+          }.bind(this)
+        } />
+        <input type="button" value="random2" onClick={
+          function() {
+            this.setState({date: (new Date().toString())})
+          }.bind(this)
+        } />
+      </div>
+    );
+  }
+}
+
+function FuncComp(props) {
+  let [ _number, setNumber ] = useState(props.initNumber);
+  let [ _date, setDate ] = useState(new Date().toString());
+  return (
+    <div className="container">
+      <h2>function style component</h2>
+      <p>Functional Component Number: {_number}</p>
+      <p>Functional Component Date: {_date}</p>
+      <input type="button" value="random1" onClick={
+        function() {
+          setNumber(Math.random());
+        }
+      } />
+      <input type="button" value="random2" onClick={
+        function() {
+          setDate(new Date().toString());
+        }
+      } />
     </div>
   );
 }
