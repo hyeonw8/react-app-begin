e03662f2d8c3d3a98f9d14b65b6c7f32d2f707bf
hyeonw8 <whgusrud223@gmail.com>
2023-05-29 01:11:40 +0900

    #1.1~2 state 사용법과 hook (수정)

--- func_class/src/App.js
+++ func_class/src/App.js
@@ -5,8 +5,35 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
-      <ClassComp initNumber={10} />
-      <FuncComp initNumber={20} />
+      <ClassComp initNumber={2} />
+      <FuncComp initNumber={2} />
+    </div>
+  );
+}
+
+function FuncComp(props) {
+  let [ _number, setNumber ] = useState(props.initNumber);
+  
+  //   const dateState = useState((new Date()).toString());
+  //   const _date = dateState[0];
+  //   const setDate = dateState[1];  
+  let [ _date, setDate ] = useState(new Date().toString());
+
+  return (
+    <div className="container">
+      <h2>function style component</h2>
+      <p> Number: {_number}</p>
+      <p> Date: {_date}</p>
+      <input type="button" value="random" onClick={
+        function() {
+          setNumber(Math.random());
+        }
+      } />
+      <input type="button" value="date" onClick={
+        function() {
+          setDate(new Date().toString());
+        }
+      } />
     </div>
   );
 }
@@ -20,14 +47,14 @@ class ClassComp extends React.Component {
     return (
       <div className="container">
         <h2>class style component</h2>
-        <p>Class Component Number: {this.state.number}</p>
-        <p>Class Component Date: {this.state.date}</p>
-        <input type="button" value="random1" onClick={
+        <p>Number: {this.state.number}</p>
+        <p>Date: {this.state.date}</p>
+        <input type="button" value="random" onClick={
           function() {
             this.setState({number: Math.random()})
           }.bind(this)
         } />
-        <input type="button" value="random2" onClick={
+        <input type="button" value="date" onClick={
           function() {
             this.setState({date: (new Date().toString())})
           }.bind(this)
@@ -35,28 +62,6 @@ class ClassComp extends React.Component {
       </div>
     );
   }
-}
-
-function FuncComp(props) {
-  let [ _number, setNumber ] = useState(props.initNumber);
-  let [ _date, setDate ] = useState(new Date().toString());
-  return (
-    <div className="container">
-      <h2>function style component</h2>
-      <p>Functional Component Number: {_number}</p>
-      <p>Functional Component Date: {_date}</p>
-      <input type="button" value="random1" onClick={
-        function() {
-          setNumber(Math.random());
-        }
-      } />
-      <input type="button" value="random2" onClick={
-        function() {
-          setDate(new Date().toString());
-        }
-      } />
-    </div>
-  );
 }
 
 export default App;
