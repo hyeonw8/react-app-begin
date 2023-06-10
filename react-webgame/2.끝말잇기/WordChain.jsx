f5473a89675671104ec20260e19852d6a33b5f1f
hyeonw8 <whgusrud223@gmail.com>
2023-06-11 00:34:16 +0900

    #1.8~9 끝말잇기 클래스 만들기, 웹팩 데브 서버와 핫 리로딩

--- react-webgame/2.끝말잇기/WordChain.jsx
+++ react-webgame/2.끝말잇기/WordChain.jsx
@@ -4,11 +4,53 @@ const {Component} = React;
 
 class WordChain extends Component {
   state = {
-    text: 'hello, webpack',
+    word: '현경',
+    value: '',
+    result: '',
+
   };
 
+  onSubmitForm = (e) => {
+    e.preventDefault();
+    if (this.state.word[this.state.word.length - 1] === this.state.value[0]) {
+      this.setState({
+        result: '딩동댕',
+        word: this.state.value,
+        value: '',
+      })
+      this.input.focus();
+    } else {
+      this.setState({
+        result: '땡',
+        value: '',
+      })
+      this.input.focus();
+    }
+  }
+
+  onChangeInput = (e) => {
+    this.setState({value:e.target.value});
+  }
+
+  input;
+  onRefInput = (c) => { //dom에 접근하는 포커스
+    this.input = c;
+  }
+
   render() {
-    return <h1>{this.state.text}</h1>
+    return (
+    <>
+      <div>{this.state.word}</div>
+      <form onSubmit={this.onSubmitForm}>
+        <input 
+         ref={this.onRefInput} 
+         value={this.state.value} 
+         type="text" 
+         onChange={this.onChangeInput} />
+        <button>입력!</button>
+      </form>
+      <div>{this.state.result}</div>
+    </>)
   }
 }
 
--- react-webgame/2.끝말잇기/webpack.config.js
+++ react-webgame/2.끝말잇기/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path'); //node
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin'); 
 
 module.exports = {
   name: 'wordchain-setting',
@@ -9,7 +10,7 @@ module.exports = {
   },
 
   entry: {
-    app: ['./client.jsx'], // 다른 파일이 불러온 파일은 안적어도됨.
+    app: ['./client'], // 다른 파일이 불러온 파일은 안적어도됨.
   }, //입력
 
   module: {
@@ -17,14 +18,31 @@ module.exports = {
       test: /\.jsx?/,
       loader: 'babel-loader',
       options: {
-        presets: ['@babel/preset-env', '@babel/preset-react'],
-        plugins: ['@babel/plugin-proposal-class-properties'],
+        presets: [
+          ['@babel/preset-env', {
+            targets: {
+              browsers: ['> 1% in KR', ], //browserslist
+            }
+          }]
+          , '@babel/preset-react'],
+        plugins: [
+          '@babel/plugin-proposal-class-properties',
+          'react-refresh/babel',
+        ],
       },
     }],
   },
 
+  plugins: [
+    new RefreshWebpackPlugin()
+  ], 
   output: {
     path: path.join(__dirname, 'dist'), // 경로를 알아서 합쳐줌
     filename: 'app.js'
   }, //출력
-};
+  devServer: {
+    devMiddleware: {publicPath: '/dist/'},
+    static: {directory: path.resolve(__dirname)},
+    hot: true
+  }
+}
