d45f7450622e7d9b1b2fd939a78b646f040112c8
hyeonw8 <whgusrud223@gmail.com>
2023-06-11 01:31:17 +0900

    #1.10~11 끝말잇기 hooks로 전환하기, 컨트롤드, 언컨트롤드 인풋 비교

--- react-webgame/2.끝말잇기/WordChain.jsx
+++ react-webgame/2.끝말잇기/WordChain.jsx
@@ -1,58 +1,73 @@
 const React = require('react');
-const {Component} = React;
+const { Component, useState, useRef } = React;
 // 필요로 하는 패키지나 라이브러리
 
-class WordChain extends Component {
-  state = {
-    word: '현경',
-    value: '',
-    result: '',
+// class WordChain extends Component {
+//   state = {
+//     word: '현경',
+//     value: '',
+//     result: '',
 
-  };
+//   };
 
-  onSubmitForm = (e) => {
+const WordChain = () => {
+  const [word, setWord] = useState('현경');
+  const [value, setValue] = useState('');
+  const [result, setResult] = useState('');
+  const inputRef = useRef(null);
+
+
+  const onSubmitForm = (e) => { //class에서 hooks로 바꾸면 얘네도 더이상 클래스 매소드가 아니기때문에 따로 바꿔줘야함
     e.preventDefault();
-    if (this.state.word[this.state.word.length - 1] === this.state.value[0]) {
-      this.setState({
-        result: '딩동댕',
-        word: this.state.value,
-        value: '',
-      })
-      this.input.focus();
+    if (word[word.length - 1] === value[0]) {
+      // this.setState({
+      //   result: '딩동댕',
+      //   word: this.state.value,
+      //   value: '',
+      // })
+      setResult('딩동댕');
+      setWord(value);
+      setValue('');
+      // this.input.focus();
+      inputRef.current.focus(); // hooks에서는 ref에 current 꼭 붙여야
     } else {
-      this.setState({
-        result: '땡',
-        value: '',
-      })
-      this.input.focus();
+      // this.setState({
+      //   result: '땡',
+      //   value: '',
+      // })
+      // this.input.focus();
+      setResult('땡');
+      setValue('');
+      inputRef.current.focus();
     }
   }
 
-  onChangeInput = (e) => {
-    this.setState({value:e.target.value});
+  const onChangeInput = (e) => {
+    // this.setState({value:e.target.value});
+    setValue(e.target.value);
   }
 
-  input;
-  onRefInput = (c) => { //dom에 접근하는 포커스
-    this.input = c;
-  }
+  // input;
+  // onRefInput = (c) => { //dom에 접근하는 포커스
+  //   this.input = c;
+  // }
 
-  render() {
-    return (
-    <>
-      <div>{this.state.word}</div>
-      <form onSubmit={this.onSubmitForm}>
+  // render() { // class에서 hooks로 바꾸면 render 필요없음
+    return (   // class에서 hooks로 바꾸면 this를 안쓰니까! this.state.word => word로
+    <>  
+      <div>{word}</div>    
+      <form onSubmit={onSubmitForm}>
         <input 
-         ref={this.onRefInput} 
-         value={this.state.value} 
+         ref={inputRef} 
+         value={value} 
          type="text" 
-         onChange={this.onChangeInput} />
+         onChange={onChangeInput} />
         <button>입력!</button>
       </form>
-      <div>{this.state.result}</div>
-    </>)
-  }
-}
+      <div>{result}</div>
+    </>);
+//   }
+  };
 
 module.exports = WordChain;
 // 지금 파일에서 쓰는 컴포넌트를 다른데서도 쓸 수 있게 
