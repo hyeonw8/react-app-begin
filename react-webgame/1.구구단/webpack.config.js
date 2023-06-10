f384b937e1187508efa4fd7aa149b0486433421c
hyeonw8 <whgusrud223@gmail.com>
2023-06-10 21:06:33 +0900

    #1.6 구구단 웹팩으로 빌드하기

--- react-webgame/1.구구단/webpack.config.js
+++ react-webgame/1.구구단/webpack.config.js
@@ -1,3 +1,30 @@
-module.exports = {
+const path = require('path');
 
+module.exports = {
+  //name: 'gugudan-setting',
+  mode: 'development',
+  devtool: 'eval', //개발 때는 eval, 배포시엔 hidden-source-map
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }, 
+
+  entry: {
+    app: ['./client'],
+  },
+
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env', '@babel/preset-react'],
+        plugins: ['@babel/plugin-proposal-class-properties'],
+      }
+    }]
+  },
+
+  output: {
+    filename: 'app.js',
+    path: path.join(__dirname, 'dist'),
+  },
 };
