2fa776e96726efd5e745ff5c974f7bb5230f00bb
hyeonw8 <whgusrud223@gmail.com>
2023-06-12 23:55:58 +0900

    #2 숫자야구 init 1-1

---
+++ react-webgame/3.숫자야구/client.jsx
@@ -0,0 +1,7 @@
+const React = require('react');
+const ReactDom = require('react-dom');
+
+const BullsAndCows = require('/BullsAndCows');
+
+ReactDom.render(<BullsAndCows />, document.querySelector('#root'));
+// <  />이런식으로 쓸거면 확장자명 jsx로~
