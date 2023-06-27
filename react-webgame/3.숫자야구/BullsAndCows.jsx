7ca09af1e5a31b42901f62a3d72055adafcd6d2a
hyeonw8 <whgusrud223@gmail.com>
2023-06-27 13:10:12 +0900

    #2.9~13 오류 수정, 주석 추가

--- react-webgame/3.숫자야구/BullsAndCows.jsx
+++ react-webgame/3.숫자야구/BullsAndCows.jsx
@@ -1,7 +1,7 @@
 import React, {useRef, useState, useCallback} from 'react';
 import Try from "./Try";
 
-const getNumbers = () => {
+function getNumbers() { // 숫자 4개를 랜덤하게 뽑는 함수, 중복없이
   const candidates = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   const array = [];
   for (let i = 0; i < 4; i += 1) {
@@ -12,9 +12,9 @@ const getNumbers = () => {
 };
 
 const BullsAndCows = () => {
-  const [answer, setAnswer] = useState(getNumbers());
   const [value, setValue] = useState('');
   const [result, setResult] = useState('');
+  const [answer, setAnswer] = useState(getNumbers); // lazy init 
   const [tries, setTries] = useState([]);
   const inputEl = useRef(null);
 
@@ -31,14 +31,14 @@ const BullsAndCows = () => {
       setResult('홈런!');
       alert('게임을 다시 실행합니다.');
       setValue('');
-      setAnswer(getNumbers());
+      setAnswer(getNumbers()); // 여긴 lazy init 아님! 호출해서 써야함
       setTries([]);
       inputEl.current.focus();
-    } else {
+    } else { // 답 틀렸으면
       const answerArray = value.split('').map((v) => parseInt(v));
       let strike = 0;
       let ball = 0;
-      if (tries.length >= 9) {
+      if (tries.length >= 9) { // 10번 이상 틀렸을 때
         setResult(`10번 넘게 틀려서 실패! 답은 ${answer.join(',')}였습니다!`); // state set은 비동기
         alert('게임을 다시 시작합니다.');
         setValue('');
@@ -47,7 +47,7 @@ const BullsAndCows = () => {
         inputEl.current.focus();
       } else {
         console.log('답은', answer.join(''));
-        for (let i = 0; i < 4; i += 1) {
+        for (let i = 0; i < 4; i += 1) { // 스트라이크와 볼 판단
           if (answerArray[i] === answer[i]) {
             console.log('strike', answerArray[i], answer[i]);
             strike += 1;
@@ -85,8 +85,8 @@ const BullsAndCows = () => {
       </form>
       <div>시도: {tries.length}</div>
       <ul>
-        {tries.map((v, i) => (
-          <Try key={`${i + 1}차 시도 : ${v.try}`} tryInfo={v}/>
+        {tries.map((v, i) => ( // 컴포넌트를 따로 분리하게 되면서 v,i가 누군지 모르니깐
+          <Try key={`${i + 1}차 시도 : ${v.try}`} tryInfo={v}/> // props로 달아줘야함, v안에 tries, result
         ))}
       </ul>
     </>
